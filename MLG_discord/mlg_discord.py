f30d3ac07eac11bf609ee10d17b34b5c773e042e
dyslexda <lexda44+github@gmail.com>
2020-07-30 03:59:18 +0000

    Added code for Discord bot

---
+++ MLG_discord/mlg_discord.py
@@ -0,0 +1,33 @@
+# sudo nohup python3 mlg_discord.py > mlg_discord.txt &
+import re, statistics, random, operator, csv, time, collections, io, string, asyncio, discord, os, sys, traceback
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
+basedir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+load_dotenv(os.path.join(basedir, '.env'))
+print(os.path.join(basedir, '.env'))
+#Discord config
+TOKEN = os.environ.get('DISCORD_TOKEN')
+GUILD = os.environ.get('DISCORD_GUILD')
+
+initial_extensions = [
+                      'cogs.owner',
+                      'cogs.dice',
+                      'cogs.gametime'
+                                   ]
+
+bot = commands.Bot(command_prefix='m!',description='MLG Discord Bot',owner_id=202278109708419072)
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+bot.run(TOKEN, bot=True, reconnect=True)
